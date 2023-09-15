901116edd15b8caf5b6d5b412aa36978adaaa4b2
DHARM PATEL <103331241+DharmPatel02@users.noreply.github.com>
2023-09-16 00:09:08 +0530

    second commit

--- server/index.js
+++ server/index.js
@@ -1,33 +1,36 @@
-const express = require('express')
-var cors = require('cors')
+const express = require("express");
+var cors = require("cors");
 var app = express();
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-require("dotenv").config({ path: './config.env' });
+require("dotenv").config({ path: "./config.env" });
 
-app.use(cors())
+app.use(cors());
 
-const connect_string = process.env.DATABASE_URI
+const connect_string = process.env.DATABASE_URI;
 
-mongoose.connect(connect_string, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
-        .then(() => console.log("Database connected!"))
-        .catch(err => console.log(err));
+mongoose
+  .connect(connect_string, {
+    ssl: true,
+    sslValidate: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database connected!"))
+  .catch((err) => console.log(err));
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json())
+app.use(express.json());
 
 // Available Routes
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
 
-app.get('/',(req,res)=>{
-  res.send("This is the backend server of iNotes website")
-})
+app.get("/", (req, res) => {
+  res.send("This is the backend server of iNotes website");
+});
 
 app.listen(PORT, () => {
-  console.log(`server is running at port no ${PORT}`)
-})
+  console.log(`server is running at port no ${PORT}`);
+});
